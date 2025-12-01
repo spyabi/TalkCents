c86ae4db4aaab18755ca0b23472b5da60d93da26
christableh <christabellim268@gmail.com>
2025-12-01 19:56:19 +0800

    fix float point bug

--- src/navigation/BottomTabs.tsx
+++ src/navigation/BottomTabs.tsx
@@ -46,9 +46,9 @@ export default function BottomTabs() {
           tabBarItemStyle: styles.tabBarItemStyle
         })}
       > 
+      <Tab.Screen name="Insights" component={InsightScreen} />
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Log" component={LogScreen} /> 
-        <Tab.Screen name="Insights" component={InsightScreen} />
         <Tab.Screen name="Settings" component={SettingScreen} />
       </Tab.Navigator>
       <FloatingButton/>
